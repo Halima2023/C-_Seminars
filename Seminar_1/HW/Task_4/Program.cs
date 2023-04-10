20deee797ff9662f251fd2d9b3976786d922cb4d
Halima <l-ahim@mail.ru>
2023-04-11 01:37:51 +0300

    HW_Task_4

---
+++ Seminar_1/HW/Task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// HW_Task_4. Задача 8: Напишите программу, которая на вход принимает  
+// число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Input a number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int evenNumber = 2;
+
+while (evenNumber < num)
+{
+    Console.Write(evenNumber + " "); evenNumber += 2;
+}
+
